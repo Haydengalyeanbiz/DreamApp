d2211ec8bbde008c70099c71ba7b9dbeb8c6d2a2
Hayden <haydengalyeanbiz@gmail.com>
2025-01-07 15:14:41 -0500

    added truncate expression to migration

--- migrations/versions/f6b611267d93_init.py
+++ migrations/versions/f6b611267d93_init.py
@@ -8,6 +8,10 @@ Create Date: 2025-01-01 22:01:17.805453
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = 'f6b611267d93'
@@ -29,6 +33,8 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
 
     op.create_table('dreams',
     sa.Column('id', sa.Integer(), nullable=False),
@@ -40,6 +46,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE dreams SET SCHEMA {SCHEMA};")
 
     op.create_table('comments',
     sa.Column('id', sa.Integer(), nullable=False),
@@ -51,6 +59,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
 
     op.create_table('likes',
     sa.Column('id', sa.Integer(), nullable=False),
@@ -60,6 +70,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE likes SET SCHEMA {SCHEMA};")
 
     # ### end Alembic commands ###
 
