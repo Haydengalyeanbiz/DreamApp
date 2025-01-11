3b13cf37c9ac2aa5ab5ad6b417452288be5c0ba3
Hayden <haydengalyeanbiz@gmail.com>
2025-01-11 00:09:36 -0500

    changed dependencies and fixed pipfile.lock

--- app/config.py
+++ app/config.py
@@ -1,17 +1,12 @@
 # app/config.py
 
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = 'Content-Type'
-
-    if os.getenv('FLASK_ENV') == 'development':
-        DEBUG = True
-        SQLALCHEMY_ECHO = True
-    else:
-        DEBUG = False
-        SQLALCHEMY_ECHO = False
