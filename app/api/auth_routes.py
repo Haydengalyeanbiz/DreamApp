ba0fdcec80479de4450e5412fb9623002eec8ab5
Hayden <haydengalyeanbiz@gmail.com>
2025-01-11 00:54:24 -0500

    removed api from auth routes

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -5,12 +5,6 @@ from flask_login import current_user, login_user, logout_user, login_required
 
 auth_routes = Blueprint('auth', __name__)
 
-@auth_routes.route('/')
-def authenticate():
-    if current_user.is_authenticated:
-        return current_user.to_dict()
-    return {'errors': {'message': 'Unauthorized'}}, 401
-
 @auth_routes.route('/login', methods=['POST'])
 def login():
     form = LoginForm()
